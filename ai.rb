0764f72eddcd5598c8469cab1f1772f7df03bd64
Magmusacy <xpfk.098@gmail.com>
2020-05-31 18:48:28 +0200

    Ai class now have methods needed for saving right guessed colors and places as well as using them in the next generated code

--- ai.rb
+++ ai.rb
@@ -1,6 +1,22 @@
-require './codemaker'
-class Ai < Codemaker # there will be module instead of inheritance in the future i think
+class Ai
+attr_reader :colors_array
+  def initialize
+    @colors_array = %w[red orange yellow green blue brown pink white black]
+  end
 
+  def codemaker(modify_array = {})
+    if modify_array.empty?
+      @colors_array.shuffle.first(4)
+    else
+      @colors_array.shuffle.first(4).map.with_index do |element,index|
+        modify_array.keys.include?(index) ? element = modify_array[index] : element 
+      end
+    end
+  end
+
+  def codecracker(code,feedback)
+    puts feedback.values
+    puts "------"
+    codemaker(feedback.map{|x,y| [x, y = code[x]]}.to_h)
+  end
 end
-
-

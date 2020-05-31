7f6f78d70c848cbc364bb5fc1427cce4db63850d
Magmusacy <xpfk.098@gmail.com>
2020-05-31 19:00:46 +0200

    Added codemaker role which lets you create your own code for computer to break

--- game.rb
+++ game.rb
@@ -1,25 +1,28 @@
 require './ai'
 require './player'
 class CheckColors
+  attr_accessor :initial_colors
   def initialize(initial_colors)
     @initial_colors = initial_colors
   end
-  def current_state(player_pick)
-    console_feedback = []
-    duplicates = find_duplicates(player_pick)
-    player_pick.each_with_index do |element,index|
+
+  def current_state(picked_colors)
+
+    console_feedback = {}
+    duplicates = find_duplicates(picked_colors)
+    picked_colors.each_with_index do |element,index|
       case
-      when @initial_colors[index] == element then console_feedback << COLOR_AND_POS
+      when initial_colors[index] == element then console_feedback[index] = COLOR_AND_POS 
       when duplicates.include?(element) then next
-      when @initial_colors.include?(element) then console_feedback << COLOR_ONLY
+      when initial_colors.include?(element) then console_feedback[index] = COLOR_ONLY
       end
     end
-    if console_feedback.select{|txt| txt == COLOR_AND_POS}.length == 4 
-      puts "The code was #{@initial_colors.join("-")}"
-      puts "Codecracker has won!!! Congratulations"
+    if console_feedback.values.select{ |txt| txt == COLOR_AND_POS }.length == 4 
+      puts "The code was #{initial_colors.join("-")}"
+      puts "Codecracker has won! Congratulations."
       won = true
     else
-      puts console_feedback
+      console_feedback
     end
   end
 
@@ -27,6 +30,7 @@ class CheckColors
 
   COLOR_AND_POS = "One of your guesses is correct both in position and color"
   COLOR_ONLY = "One of your guesses is correct in color but on the wrong position"
+
   def find_duplicates(array)
     duplicates = Hash.new(0)
     array.each do |element|
@@ -34,6 +38,7 @@ class CheckColors
     end
     duplicates.select{ |key,val| key if val > 1}.keys
   end
+
 end
 
 puts "Welcome to mastermind game"
@@ -45,21 +50,38 @@ role = gets.chomp.downcase
   end
 
 computer = Ai.new
-computer_pick = computer.auto_pick_colors
-p computer_pick
-colors_check = CheckColors.new(computer_pick)
 
-puts "Available colors to choose from are: [#{Codemaker.get_colors.join(", ")}]"
-for i in 1..13
-  if i == 13 # the game is set up for 12 rounds so 13th round without code cracked equals to codecracker's loss 
-    puts "Codecracker lost. Codemaker made the code too hard"
-    break
-  end
-  puts "Please type four different colors (dash-separated)"
-  player_pick = gets.chomp.downcase.split("-")
-    until player_pick.length == 4 do
-      puts "Please type four different colors (dash-separated)"
-      player_pick = gets.chomp.downcase.split("-")
+if role == "codemaker" 
+  available_colors = %w[red orange yellow green blue brown pink white black]
+  puts "Pick 4 colors from this array (separate each color using dash-):"
+  puts "[#{available_colors.join(", ")}]"
+  chosen_colors = gets.chomp.downcase.split("-")
+    until chosen_colors.all?{ |colors| available_colors.include?(colors)} do # checks if all colors in chosen_colors are included in available_colors
+      puts "Something went wrong... Try again"
+      chosen_colors = gets.chomp.downcase.split("-")
     end
-  break if colors_check.current_state(player_pick) == true
+else 
+  chosen_colors = computer.codemaker
+end
+
+colors_check = CheckColors.new(chosen_colors)
+
+puts "Available colors to choose from are: [#{Ai.new.colors_array}]"
+
+i = 0
+while i < 13
+  if role == "codecracker"
+    code = gets.chomp.downcase.split("-")
+      until code.length == 4 do
+        puts "Please type four different colors (dash-separated)"
+        code = gets.chomp.downcase.split("-")
+      end
+  else
+    code ||= computer.codemaker
+    code = computer.codecracker(code, colors_check.current_state(code))
+    puts "Computer's guess is #{code}."
+  end
+break if colors_check.current_state(code) == true 
+puts "The code was too hard for codecracker to crack. Codemaker won this game!" if i == 12
+i += 1
 end
